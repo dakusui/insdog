6f8dc7033f0ee96381dc727f7bc4ab5143d89993
Hiroshi Ukai <dakusui@gmail.com>
2025-04-20 19:33:20 +0900

    improve: Error message at execution time

--- src/main/java/jp/co/moneyforward/autotest/framework/action/ActionComposer.java
+++ src/main/java/jp/co/moneyforward/autotest/framework/action/ActionComposer.java
@@ -3,13 +3,17 @@ package jp.co.moneyforward.autotest.framework.action;
 import com.github.dakusui.actionunit.actions.Ensured;
 import com.github.dakusui.actionunit.core.Action;
 import com.github.dakusui.actionunit.core.Context;
+import com.github.valid8j.pcond.forms.Printables;
+import jp.co.moneyforward.autotest.framework.core.AutotestException;
 import jp.co.moneyforward.autotest.framework.core.ExecutionEnvironment;
 import jp.co.moneyforward.autotest.framework.internal.InternalUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.stream.Stream;
@@ -18,7 +22,6 @@ import static com.github.dakusui.actionunit.core.ActionSupport.*;
 import static com.github.valid8j.classic.Requires.requireNonNull;
 import static jp.co.moneyforward.autotest.framework.internal.InternalUtils.concat;
 
-///
 /// An interface that models a factory of actions.
 ///
 /// This interface is designed to be a "visitor" of "calls", each of which represents a call of an action (`ActionFactory`).
@@ -39,35 +42,26 @@ import static jp.co.moneyforward.autotest.framework.internal.InternalUtils.conca
 /// @see Call
 /// @see Scene
 /// @see Act
-///
 public interface ActionComposer {
-  ///
   /// A logger object
-  ///
   Logger LOGGER = LoggerFactory.getLogger(ActionComposer.class);
   
-  ///
   /// Returns currently ongoing `SceneCall` object.
   ///
   /// @return Currently ongoing `SceneCall` object.
-  ///
   SceneCall ongoingSceneCall();
   
-  ///
   /// Returns an execution environment in which actions created by this composer objects are performed.
   ///
   /// @return An execution environment.
-  ///
   ExecutionEnvironment executionEnvironment();
   
   List<String> ongoingWorkingVariableStoreNames();
   
-  ///
   /// Creates an action for a given `SceneCall` object.
   ///
   /// @param sceneCall A scene call from which an action should be created.
   /// @return A sequential action created from `sceneCall`.
-  ///
   default Action create(SceneCall sceneCall) {
     return sequential(concat(Stream.of(sceneCall.begin(ongoingWorkingVariableStoreNames())),
                              InternalUtils.flattenSequentialAction(sceneCall.targetScene().toSequentialAction(this)),
@@ -113,7 +107,7 @@ public interface ActionComposer {
   private static <T, R> Consumer<Context> toContextConsumerFromAct(SceneCall ongoingSceneCall,
                                                                    ActCall<T, R> actCall,
                                                                    ExecutionEnvironment executionEnvironment) {
-    return toContextConsumerFromAct(c -> actCall.resolveVariable(ongoingSceneCall, c),
+    return toContextConsumerFromAct(Printables.function(actCall.inputVariableName(), c -> actCall.resolveVariable(ongoingSceneCall, c)),
                                     actCall.act(),
                                     actCall.outputVariableName(),
                                     ongoingSceneCall,
@@ -129,14 +123,24 @@ public interface ActionComposer {
       String targetSceneName = ongoingSceneCall.targetScene().name();
       String actName = act.name();
       LOGGER.debug("ENTERING: {}:{}", targetSceneName, actName);
+      Map<String, Object> workingVariableStore = ongoingSceneCall.workingVariableStore(c);
       try {
         var v = act.perform(inputVariableResolver.apply(c), executionEnvironment);
-        ongoingSceneCall.workingVariableStore(c)
-                        .put(outputVariableName, v);
+        workingVariableStore.put(outputVariableName, v);
       } catch (Error | RuntimeException e) {
-        LOGGER.error(e.getMessage());
-        LOGGER.debug(e.getMessage(), e);
-        throw e;
+        String message = (MessageFormat.format("Scene<{0}>.Act<{1}>[{2}]: available variables are: {3}",
+                                                ongoingSceneCall.outputVariableStoreName(),
+                                                act.name(),
+                                                inputVariableResolver.toString(),
+                                                workingVariableStore.keySet()
+                                                                    .stream()
+                                                                    .sorted()
+                                                                    .map(k -> MessageFormat.format("{0}={1}",
+                                                                                                   k,
+                                                                                                   workingVariableStore.get(k)))
+                                                                    .toList()));
+        
+        throw new AutotestException(message + ":[" + e.getMessage() + "]", e);
       } finally {
         LOGGER.debug("LEAVING:  {}:{}", targetSceneName, actName);
       }
