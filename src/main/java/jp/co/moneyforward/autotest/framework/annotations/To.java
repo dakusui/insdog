0f0da15057a236a714fc78b1ac40a60fb60bf55b
Hiroshi Ukai <dakusui@gmail.com>
2025-04-27 18:02:15 +0900

    doc: Mention @Given and @To in package-info.md - Also, add JavaDoc to @To annotation.

--- src/main/java/jp/co/moneyforward/autotest/framework/annotations/To.java
+++ src/main/java/jp/co/moneyforward/autotest/framework/annotations/To.java
@@ -6,8 +6,11 @@ import java.lang.annotation.Target;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
+///  An annotation that indicates a name of a variable to which the value returned by the method is assigned.
+///  This annotation is attached to a method that returns a non-`Scene` value.
 @Retention(RUNTIME)
 @Target(METHOD)
 public @interface To {
+  /// @return A name of the variable to which the value returned by the annotated method is assigned.
   String value();
 }
